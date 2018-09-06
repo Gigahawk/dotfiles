b0185696b923fcd76b6be86738f561da754a7262
Jasper Chan <jasperchan515@gmail.com>
2018-09-06 16:21:39 -0700

    Add qutebrowser settings

---
+++ qutebrowser/config.py
@@ -0,0 +1,17 @@
+# DO NOT DELETE LMAO
+config.bind(';h', 'set downloads.location.directory ~ ;; hint links download')
+config.bind(';d', 'set downloads.location.directory ~/Documents ;; hint links download')
+config.bind(';D', 'set downloads.location.directory ~/Downloads ;; hint links download')
+config.bind(';pp', 'set downloads.location.directory ~/Pictures ;; hint links download')
+config.bind(';vv', 'set downloads.location.directory ~/Videos ;; hint links download')
+config.bind(';m', 'set downloads.location.directory ~/Music ;; hint links download')
+config.bind(';b', 'set downloads.location.directory ~/Books ;; hint links download')
+config.bind(';s', 'set downloads.location.directory ~/.config/Scripts ;; hint links download')
+config.bind(';r', 'set downloads.location.directory / ;; hint links download')
+config.bind(';cf', 'set downloads.location.directory ~/.config ;; hint links download')
+
+c.url.start_pages = "https://www.google.com"
+c.url.searchengines = {
+    "DEFAULT": "https://www.google.com/search?hl=en&q={}"
+}
+# DO NOT DELETE LMAO
